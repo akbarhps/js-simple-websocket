64a0762f9b483079cae85e15caa06e6e170b9009
akbarhps <akbarputrasrg@gmail.com>
2021-02-15 16:07:23 +0700

    🧹 Add more log on events

--- index.js
+++ index.js
@@ -18,11 +18,13 @@ io.on('connection', (socket) => {
 
   // Handle chat event
   socket.on('chat', function (data) {
+    console.log(data);
     io.sockets.emit('chat', data);
   });
 
   // Handle typing event
   socket.on('typing', function (data) {
+    console.log(data);
     socket.broadcast.emit('typing', data);
   });
 
