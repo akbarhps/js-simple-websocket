5a7ac035078914526f4e38b742430eae59393b84
akbarhps <akbarputrasrg@gmail.com>
2021-02-15 16:13:14 +0700

    🧹 Change io .connect to deploy url

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-const socket = io.connect("http://localhost:3000/");
+const socket = io.connect("window.location.origin");
 
 let message = document.getElementById('message'),
   handle = document.getElementById('handle'),
